f7515f499e6a616ee79d3f8d8997418b34d3a86e
ClintonMoshe <45009387+clinton-mwachia@users.noreply.github.com>
2020-02-22 12:44:40 +0300

    TWITTER-API-TOKENS

---
+++ API.R
@@ -0,0 +1,13 @@
+# API AND TOKENS
+api_key = "JBYnhendu0mNY4sReT4blmrJ1"
+api_secret_key = "cD3ZYHaUv1c7uvgAuB1g8Qxm4xuZHDx7Ega48dYmF81QbRmy1g"
+access_token = "987617080961839104-9zd5SyW28sS6kV8oRNdNleaoC5eCF6O"
+access_token_secret = "uzmK5sB9aSDvSQtdWlugVftUO4hRG0EKDwS7TFAsL7ooO"
+
+
+token <- create_token(
+  app = "text-analysis-twitter",
+  consumer_key = api_key,
+  consumer_secret = api_secret_key,
+  access_token = access_token,
+  access_secret = access_token_secret)
